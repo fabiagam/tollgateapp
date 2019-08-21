90d00bba6017bdfc333fee14256c4980a9c40201
James N. Abiagam <developer@qwikpay.com.ng>
2019-08-22 00:06:14 +0100

    Added footer layout

---
+++ components/layouts/Footer.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default() => (
+        <div>
+          <div className="container-fluid  footer">
+              <div className="container">
+                  <div className="row">
+                      <div className="col-md-12 col-lg-12 text-center"><p>&copy; Copyright 2019. All rights reserved. </p></div>
+                  </div>
+              </div>
+          </div>  
+        </div>
+);
